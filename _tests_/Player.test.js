5c87cc2233050cd2088086459826c66f005896e8
Sara <sara.m.adamski@gmail.com>
2021-05-18 14:52:21 -0500

    prototype methods for player health

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -4,8 +4,6 @@ const Potion = require('../lib/Potion.js');
 // mocks/replaces the constructor's implementation with our faked data
 jest.mock('../lib/Potion.js');
 
-console.log(new Potion());
-
 test('creates a player object', () => {
     // hard code player name to be Dave
     const player = new Player('Dave');
@@ -36,4 +34,31 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
     // if inventory is empty, return false
     expect(player.getInventory()).toEqual(false);
+})
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+    // get information about player's health and check that string contains player's health
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+})
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+    // check if player is alive (player alive = true)
+    expect(player.isAlive()).toBeTruthy();
+    // if player is dead, set player health equal to 0 before running next expect
+    player.health = 0;
+    // if player health is 0, check to see if player is dead (player alive = false)
+    expect(player.isAlive()).toBeFalsy();
+})
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+    // call reduceHealth() function to subtract 5 points from health
+    player.reduceHealth(5);
+    expect(player.health).toBe(oldHealth - 5);
+    // call reduceHealth() function to subtract a high number to ensure that health cannot go negative
+    player.reduceHealth(99999);
+    expect(player.health).toBe(0);
 })
